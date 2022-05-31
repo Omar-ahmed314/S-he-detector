810202944c38e423bf23c34785a21748895fb4f3
mariemzayn18 <mariamzayn18@gmail.com>
2022-05-31 04:39:12 +0200

    Validations for prediction

--- predict.py
+++ predict.py
@@ -54,31 +54,42 @@ file2.truncate(0)
 
 i = 1
 for file in files:
+
+    try:
+        img = cv2.imread(testPath+'/'+ file )
+
+        start = timeit.default_timer() 
+
+        cropped_img = preprocessing.cropTxtOnly(img)
+
+        preprocessed_img = preprocessing.preprocessing(cropped_img)
+
+        cv2.imwrite("testing/test.jpg", preprocessed_img)
+
+        X_test = ExtracteFeatures.extract(cold)
+
+        Y_Predicted=clf.predict([X_test])
+
+    except: 
+        Y_Predicted=-1
     
-    img = cv2.imread(testPath+'/'+ file )
-
-    start = timeit.default_timer() 
-
-    cropped_img = preprocessing.cropTxtOnly(img)
-
-    preprocessed_img = preprocessing.preprocessing(cropped_img)
-
-    cv2.imwrite("testing/test.jpg", preprocessed_img)
-
-    X_test = ExtracteFeatures.extract(cold)
-
-    Y_Predicted=clf.predict([X_test])
-
     end= timeit.default_timer()  - start
-
-    if(end == 0):
-        end =  0.001
+   
+    
     if( i == len(files)):
         file1.write(str(int(Y_Predicted)))
-        file2.write(str(round(end,2)))
+        if(end == 0):
+            end =  0.001
+            file2.write(str(end))
+        else: 
+            file2.write(str(round(end,2)))
     else:
         file1.write(str(int(Y_Predicted))+ "\n")
-        file2.write(str(round(end,2))+ "\n")
+        if(end == 0):
+            end =  0.001
+            file2.write(str(end)+ "\n")
+        else: 
+            file2.write(str(round(end,2))+ "\n")
     i+=1
      
 file1.close() 
