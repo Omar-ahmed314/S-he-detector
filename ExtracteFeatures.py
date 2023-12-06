0fe0c0c3e68ff9e2b68ff85dfa898343e625405b
abbas <62177195+Omar-ahmed314@users.noreply.github.com>
2023-12-06 18:14:14 +0200

    fixing predict model

--- ExtracteFeatures.py
+++ ExtracteFeatures.py
@@ -1,94 +1,101 @@
 from skimage.feature import hog
 from skimage.transform import resize
-import numpy as np 
+import numpy as np
 import os
-from  LBP_descriptor import LocalBinaryPatterns
+from LBP_descriptor import LocalBinaryPatterns
 import commonfunctions as cf
+from cold_feature import cold_feature
 import cv2
 import csv
 from skimage.feature import greycomatrix, greycoprops
 
 
-
-# ICDAR LABELS 
-labels_ICDAR=[]
-with open("our dataset/train_answers.csv", 'r') as file:
-    csvreader = csv.reader(file, delimiter=',')
-    rows= np.array(list(csvreader))[1:].astype(float).astype(int)
+# ICDAR LABELS
+labels_ICDAR = []
+with open("our dataset/train_answers.csv", "r") as file:
+    csvreader = csv.reader(file, delimiter=",")
+    rows = np.array(list(csvreader))[1:].astype(float).astype(int)
 for row in rows:
     labels_ICDAR.append(row[1])
-    
+
+
 def get_label_ICDAR(img):
-    if img[0]=='0':
-        if img[1]=='0': 
-            return labels_ICDAR[int(img[2])-1]
-        else: 
-            return labels_ICDAR[int(img[1:3])-1]
-    else: 
-         return labels_ICDAR[int(img[0:3])-1]
-        
-def read_labels(path): 
-    y=[]
+    if img[0] == "0":
+        if img[1] == "0":
+            return labels_ICDAR[int(img[2]) - 1]
+        else:
+            return labels_ICDAR[int(img[1:3]) - 1]
+    else:
+        return labels_ICDAR[int(img[0:3]) - 1]
+
+
+def read_labels(path):
+    y = []
     files = os.listdir(path)
 
     for file in files:
-        if file[0]=='F':
+        if file[0] == "F":
             y.append(0)
-        elif file[0]=='M':
+        elif file[0] == "M":
             y.append(1)
-        else: # ICDAR dataset
+        else:  # ICDAR dataset
             y.append(get_label_ICDAR(file[1:4]))
-    y=np.array(y).astype(float)
-    return y 
+    y = np.array(y).astype(float)
+    return y
+
 
 def HOG(img):
-    img = np.array(resize(img,(128,64))) 
-    feature_vector, hog_image = hog(img, orientations=9, pixels_per_cell=(8,8), cells_per_block=(3,3), visualize=True)
-    return feature_vector,hog_image
+    img = np.array(resize(img, (128, 64)))
+    feature_vector, hog_image = hog(
+        img,
+        orientations=9,
+        pixels_per_cell=(8, 8),
+        cells_per_block=(3, 3),
+        visualize=True,
+    )
+    return feature_vector, hog_image
+
 
 def lbp():
     return LocalBinaryPatterns(24, 8)
 
-def GLCM (image):
 
+def GLCM(image):
     # convert image to gray
-    image= cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    featureVector=[]
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    featureVector = []
 
-    glcm = greycomatrix(image, distances=[5], angles=[0], levels=256,
-                            symmetric=True, normed=True)
-    featureVector.append(greycoprops(glcm, 'dissimilarity')[0, 0])
-    featureVector.append(greycoprops(glcm, 'correlation')[0, 0])
-    featureVector.append(greycoprops(glcm, 'contrast')[0, 0])
-    featureVector.append(greycoprops(glcm, 'homogeneity')[0, 0])
+    glcm = greycomatrix(
+        image, distances=[5], angles=[0], levels=256, symmetric=True, normed=True
+    )
+    featureVector.append(greycoprops(glcm, "dissimilarity")[0, 0])
+    featureVector.append(greycoprops(glcm, "correlation")[0, 0])
+    featureVector.append(greycoprops(glcm, "contrast")[0, 0])
+    featureVector.append(greycoprops(glcm, "homogeneity")[0, 0])
     return featureVector
 
-def extract(cold):
 
+def extract(image):
     # read the image
     # img = cv2.imread("Test_data/"+file )
-    # ------------------- Convert into gray image -------------
-    # convert to gray
-    image = cv2.imread("testing/test.jpg" )
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        
+    # cold feature
+    cold = cold_feature()
     # threshold the grayscale image
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
+    thresh = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
     # ------------------- GLCM feature-----------------------
-    GLCM_train=GLCM(image)
-    #--------------------------------------------------------
-    # ------------------- HOG feature------------------------
-    HOG_train,hog_image=HOG(thresh)
+    GLCM_train = GLCM(image)
     # --------------------------------------------------------
-    #------------------- LBP feature------------------------
+    # ------------------- HOG feature------------------------
+    HOG_train, hog_image = HOG(thresh)
+    # --------------------------------------------------------
+    # ------------------- LBP feature------------------------
     hist = lbp().describe(thresh)
-    #-------------------- Cold feature ------------------------
+    # -------------------- Cold feature ------------------------
     cold_feature_vector = cold.getFeatureVectors(thresh)
 
-    # concatenate all the features in X_train   
-    feature_test_temp=np.hstack((HOG_train, GLCM_train, hist, cold_feature_vector)).tolist()
- 
-    
+    # concatenate all the features in X_train
+    feature_test_temp = np.hstack(
+        (HOG_train, GLCM_train, hist, cold_feature_vector)
+    ).tolist()
+
     return feature_test_temp
- 
--- predict.py
+++ predict.py
@@ -5,8 +5,7 @@ import os
 import cv2
 import timeit
 import numpy as np
-import Classification
-from cold_feature import cold_feature
+import pickle
 
 parser = argparse.ArgumentParser()
 parser.add_argument("testPath", help="path")
@@ -15,95 +14,53 @@ parser.add_argument("outPath", help="path")
 args = parser.parse_args()
 testPath = args.testPath
 outPath = args.outPath
-# target = "Test_data_evaluation/"
-#-------------------delete---------------------------------------------
-# Y_train= ExtracteFeatures.read_labels("Training_data/")
 
-# #write train labels in external file
-# with open('final models/85.13/final_train_labels.npy', 'wb') as f:
-#     np.save(f, Y_train)
-# f.close()  
-#------------------------delete-------------------------------------------
-# then uncomment this:
-# # Read feature vector of train data from the npy file 
-with open('final models/85.13/final_train_labels.npy', 'rb') as f:
-    Y_train = np.load(f,allow_pickle=True)
-f.close() 
-#-------------------------------------------------------------------
+clf = pickle.load(open("final models/85.13/Classification_Model.pkl", "rb"))
 
-# Read feature vector of train data from the npy file 
-with open('final models/85.13/ALL_features.npy', 'rb') as f:
-    X_train = np.load(f,allow_pickle=True)
-f.close() 
-# print(len(X_train[0]))
-
-# files=os.listdir(testPath)
-# for file in files:
-#     img = cv2.imread("Training_data/"+file )  
-#     X_train = ExtracteFeatures.extract(img,file)
-# #write feature vector of each image in external file
-# with open('HOG_LBP_GLCM_COLD_train.npy', 'wb') as f:
-#     np.save(f, X_train)
-# f.close()
-
-clf = Classification.Gradient_Boost_Classifier(X_train,Y_train)
-
-# with open('clf.npy', 'wb') as f:
-#     np.save(f, clf)
-# f.close()
-
-# with open('clf.npy', 'rb') as f:
-#     clf = np.load(f,allow_pickle=True)
-# f.close() 
-
-cold = cold_feature()
-# print("hi")
-files=os.listdir(testPath)
-file1 = open(outPath+'/results.txt', 'a')
+files = os.listdir(testPath)
+print(len(files))
+file1 = open(outPath + "/results.txt", "w")
 file1.truncate(0)
 
-file2 = open(outPath+'/times.txt', 'a')
+file2 = open(outPath + "/times.txt", "w")
 file2.truncate(0)
 
 i = 1
 for file in files:
-
     try:
-        img = cv2.imread(testPath+'/'+ file )
+        img = cv2.imread(testPath + "/" + file)
 
-        start = timeit.default_timer() 
+        start = timeit.default_timer()
 
         cropped_img = preprocessing.cropTxtOnly(img)
 
         preprocessed_img = preprocessing.preprocessing(cropped_img)
 
-        cv2.imwrite("testing/test.jpg", preprocessed_img)
+        X_test = ExtracteFeatures.extract(preprocessed_img)
 
-        X_test = ExtracteFeatures.extract(cold)
+        Y_Predicted = clf.predict([X_test])
 
-        Y_Predicted=clf.predict([X_test])
+    except Exception as err:
+        print("Exception error", err)
+        Y_Predicted = -1
 
-    except: 
-        Y_Predicted=-1
-    
-    end= timeit.default_timer()  - start
-   
-    
-    if( i == len(files)):
+    end = timeit.default_timer() - start
+
+    if i == len(files):
         file1.write(str(int(Y_Predicted)))
-        if(round(end,3) == 0.0):
-            end =  0.001
+        if round(end, 3) == 0.0:
+            end = 0.001
             file2.write(str(end))
-        else: 
-            file2.write(str(round(end,2)))
+        else:
+            file2.write(str(round(end, 2)))
     else:
-        file1.write(str(int(Y_Predicted))+ "\n")
-        if(round(end,3) == 0.0):
-            end =  0.001
-            file2.write(str(end)+ "\n")
-        else: 
-            file2.write(str(round(end,2))+ "\n")
-    i+=1
-     
-file1.close() 
-file2.close() 
+        file1.write(str(int(Y_Predicted)) + "\n")
+        if round(end, 3) == 0.0:
+            end = 0.001
+            file2.write(str(end) + "\n")
+        else:
+            file2.write(str(round(end, 2)) + "\n")
+    i += 1
+
+file1.close()
+file2.close()
